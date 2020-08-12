abb8907737d919965a9134b6bffe8631f6759874
himanshusuman555 <46639298+himanshusuman555@users.noreply.github.com>
2020-08-12 22:34:48 +0530

    Add files via upload

---
+++ Test5.java
@@ -0,0 +1,11 @@
+class Test5
+{
+  public static void main(String args[])
+   {
+      int a=5,b=6;
+      int c=a+b;
+      System.out.println("a + b ="+c);
+      System.out.println(a + "+" + b +"=" +c);
+      System.out.println("Sum of " +a+ "+" +b +"="+c); 
+   }
+}
