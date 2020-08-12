2ccb7b2817948402343ebb893ea359999af7aaf5
himanshusuman555 <46639298+himanshusuman555@users.noreply.github.com>
2020-08-12 23:00:07 +0530

    Add files via upload

---
+++ Arith2.java
@@ -0,0 +1,12 @@
+class Arith2
+{
+	public static void main(String args[])
+	{
+		int a=5,b=7;
+		System.out.println(a+ "+" +b + "="+(a+b));
+		System.out.println(a + "-" + b + "="+(a-b));
+		System.out.println(a + "*" + b + "="+(a*b));
+		System.out.println(a + "/" + b + "="+(a/b));
+		System.out.println(a + "%" + b + "="+(a%b));
+	}
+}
