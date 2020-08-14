79754249501370c44d59058725c8547085111d9a
himanshusuman555 <46639298+himanshusuman555@users.noreply.github.com>
2020-08-14 09:17:14 +0530

    Add files via upload

---
+++ Arithm.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class Arithm
+{
+	 public static void main(String args[])
+	 {
+		 int a,b;
+		 a=5;
+		 b=7;
+		 System.out.println("Addition="+(a+b)+"\nSubstraction="+(a-b)+"\nDivision="+(a/b)+"\nMultiplicATION="+(a*b)+"\nModulas="+(a%b));
+	 }
+}	 
